a3842d059ac2003236edb1ea66da4f503271c90f
abhishek16a <abhishekadhikari16a@gmail.com>
2019-04-10 22:03:56 +0545

    adding scrollview in the design

---
+++ app/src/main/java/com/example/typeandrun/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.typeandrun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView tvOutput;
+    private Button btnCreateCar;
+    private EditText etManufacturer, etYear, etColor, etPrice, etEngineSize;
+    
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
