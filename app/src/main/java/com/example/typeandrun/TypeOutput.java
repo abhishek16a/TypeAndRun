c880d53c7bff4bbc447b95034da0d96d631e7683
abhishek16a <abhishekadhikari16a@gmail.com>
2019-04-10 23:00:53 +0545

    getter and setter and making class and object

---
+++ app/src/main/java/com/example/typeandrun/TypeOutput.java
@@ -0,0 +1,50 @@
+package com.example.typeandrun;
+
+public class TypeOutput extends MainActivity {
+
+    private String manufacturer;
+    private Integer year;
+    private String color;
+    private Float Price;
+    private Integer engineSize;
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Float getPrice() {
+        return Price;
+    }
+
+    public void setPrice(Float price) {
+        Price = price;
+    }
+
+    public Integer getEngineSize() {
+        return engineSize;
+    }
+
+    public void setEngineSize(Integer engineSize) {
+        this.engineSize = engineSize;
+    }
+}
