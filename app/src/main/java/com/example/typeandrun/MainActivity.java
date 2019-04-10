f45baaef938a13dd981466865cc431619ac8ba93
abhishek16a <abhishekadhikari16a@gmail.com>
2019-04-10 23:00:02 +0545

    getter and setter and making class and object

--- app/src/main/java/com/example/typeandrun/MainActivity.java
+++ app/src/main/java/com/example/typeandrun/MainActivity.java
@@ -12,17 +12,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView tvOutput;
     private Button btnCreateCar;
     private EditText etManufacturer, etYear, etColor, etPrice, etEngineSize;
-    
+    int i=0;
+    TypeOutput typeOutput=new TypeOutput();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etManufacturer=findViewById(R.id.etManufacturer);
+        etYear=findViewById(R.id.etYear);
+        etColor=findViewById(R.id.etColor);
+        etPrice=findViewById(R.id.etPrice);
+        etEngineSize=findViewById(R.id.etEngineSize);
+        tvOutput=findViewById(R.id.tvOutput);
+
+        btnCreateCar.setOnClickListener(this);
+
+
     }
 
     @Override
     public void onClick(View v) {
+
+     typeOutput.setManufacturer(etManufacturer.getText().toString());
+     typeOutput.setYear(Integer.parseInt(etYear.getText().toString()));
+     typeOutput.setColor(etColor.getText().toString());
+     typeOutput.setPrice(Float.parseFloat(etPrice.getText().toString()));
+     typeOutput.setEngineSize(Integer.parseInt(etEngineSize.getText().toString()));
+
+     tvOutput.append("This is Vehicle No " +i+ "\n" + "Manufactured by" +etManufacturer+ "\n");
 
     }
 }
